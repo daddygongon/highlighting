dacd82d73e9d685a045e4d23a6c105586a140399
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-11-03 18:43:57 +0900

    rev for win10

--- lib/highlighting.rb
+++ lib/highlighting.rb
@@ -10,18 +10,26 @@ module Highlighting
   module_function
   
   def select_theme
-    res = command_line "defaults read -g AppleInterfaceStyle"
-    theme = if res.stdout.chomp == 'Dark'
-              'andes.theme'
-            else
-              'seashell.theme'
-            end
+    if  ENV["TERM_PROGRAM"]=="Apple_Terminal"
+      res = command_line "defaults read -g AppleInterfaceStyle"
+      theme = if res.stdout.chomp == 'Dark'
+                'andes.theme'
+              else
+                'seashell.theme'
+              end
+    else
+      theme = 'andes.theme'
+    end
     return File.join(File.dirname(__FILE__), 'themes', theme)
   end
 
   # puts "Hello world."
   # p ARGV
   file = ARGV[0]
+  if file == nil
+    puts "No file assigned."
+    exit
+  end
   theme = select_theme()
   # system "which highlight"
   comm = "highlight -O xterm256 #{file} --config-file=#{theme}"
